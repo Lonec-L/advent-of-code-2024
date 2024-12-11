d5aaa011b62cf3379476a8ef5814d888459d7994
Luka Lonec <luka.lonec@gmail.com>
2024-12-11 14:22:03 +0100

    Very bad day6 part2 solution
    Not proud of this one, but I do not want to spend more time coming up
    with a smarter solution. It is what it is, on to the next one.

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,6 @@
 use std::process::exit;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 enum Direction {
     UP,
     RIGHT,
@@ -32,6 +32,7 @@ pub fn part1(input: &str) -> u64 {
         for j in 0..data[i].len() {
             if data[i][j] == '^' {
                 (x, y) = (i, j);
+                data[x][y] = 'X';
             }
         }
     }
@@ -49,14 +50,14 @@ pub fn part1(input: &str) -> u64 {
                 ny = y;
             }
             Direction::RIGHT => {
-                if y as i32 + 1 < 0 {
+                if y as i32 + 1 >= data[x].len() as i32 {
                     break;
                 }
                 nx = x;
                 ny = y + 1;
             }
             Direction::DOWN => {
-                if x as i32 + 1 < 0 {
+                if x as i32 + 1 >= data.len() as i32 {
                     break;
                 }
                 nx = x + 1;
@@ -89,36 +90,25 @@ pub fn part1(input: &str) -> u64 {
     res
 }
 
+// I am not proud of this solution, it is slow and inefficient. It is a limited brute force, and a
+// full brute force for loop checking (just counting that we didi way too many steps). This
+// shouldve been done in a smarter way, and code should not be duplicated.
+//
+// Anyhow, I spent way too much time on this problem, so this will have to do, for now.
 #[aoc(day6, part2)]
 pub fn part2(input: &str) -> u32 {
-    //if guard is on the already visited square, and turning him
-    //right would put point him in the same direction that he was turned at the last time he was on
-    //that square, then this is a possible loop. Need to also count like he walked up to the starting position
-    //
-    //we need one array to store all directions he was turned (can be out main array, just use some
-    //chars to represent directions), and another one to store obstacles, to not count obstacles
-    //twice
-
-    let mut res = 0;
-
     let mut data: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
-
-    let mut obstacles = data.clone();
 
     let (mut x, mut y): (usize, usize) = (0, 0);
 
-    'find: for i in 0..data.len() {
+    let (mut bx, mut by): (usize, usize) = (0, 0);
+
+    for i in 0..data.len() {
         for j in 0..data[i].len() {
             if data[i][j] == '^' {
                 (x, y) = (i, j);
-
-                for k in x..data.len() {
-                    match data[k][y] {
-                        '#' => break,
-                        _ => data[k][y] = 'u',
-                    }
-                }
-                break 'find;
+                (bx, by) = (i, j);
+                data[x][y] = 'X';
             }
         }
     }
@@ -136,14 +126,14 @@ pub fn part2(input: &str) -> u32 {
                 ny = y;
             }
             Direction::RIGHT => {
-                if y as i32 + 1 < 0 {
+                if y as i32 + 1 >= data[x].len() as i32 {
                     break;
                 }
                 nx = x;
                 ny = y + 1;
             }
             Direction::DOWN => {
-                if x as i32 + 1 < 0 {
+                if x as i32 + 1 >= data.len() as i32 {
                     break;
                 }
                 nx = x + 1;
@@ -157,43 +147,80 @@ pub fn part2(input: &str) -> u32 {
                 ny = y - 1;
             }
         }
-
-        use Direction::*;
         match data[nx][ny] {
-            '#' => {
-                d = d.turn();
-                data[x][y] = match d {
-                    LEFT => 'l',
-                    RIGHT => 'r',
-                    UP => 'u',
-                    DOWN => 'd',
-                };
-            }
-            _ => {
-                if obstacles[nx][ny] != '#' {
-                    println!("{}, {:?}, {}", data[x][y], d.turn(), obstacles[nx][ny]);
-                    match (d.turn(), data[x][y]) {
-                        (LEFT, 'l') | (RIGHT, 'r') | (UP, 'u') | (DOWN, 'd') => {
-                            res += 1;
-                            obstacles[nx][ny] = '#';
-                        }
-                        _ => (),
-                    }
-                }
-                data[x][y] = match d {
-                    LEFT => 'l',
-                    RIGHT => 'r',
-                    UP => 'u',
-                    DOWN => 'd',
-                };
+            '#' => d = d.turn(),
+            'X' => {
                 x = nx;
                 y = ny;
             }
+            '.' => {
+                x = nx;
+                y = ny;
+                data[x][y] = 'X';
+            }
+            _ => exit(1),
         }
     }
-    let v: Vec<String> = data.into_iter().map(|v| String::from_iter(v)).collect();
-    for l in v {
-        println!("{:?}", l);
+
+    let mut res = 0;
+    for i in 0..data.len() {
+        for j in 0..data[i].len() {
+            if data[i][j] != 'X' || (i, j) == (bx, by) {
+                continue;
+            }
+            let mut inner_data = data.clone();
+            inner_data[i][j] = '#';
+            (x, y) = (bx, by);
+            d = Direction::UP;
+            let mut steps: usize = 0;
+            loop {
+                match d {
+                    Direction::UP => {
+                        if x as i32 - 1 < 0 {
+                            break;
+                        }
+                        nx = x - 1;
+                        ny = y;
+                    }
+                    Direction::RIGHT => {
+                        if y as i32 + 1 >= data[x].len() as i32 {
+                            break;
+                        }
+                        nx = x;
+                        ny = y + 1;
+                    }
+                    Direction::DOWN => {
+                        if x as i32 + 1 >= data.len() as i32 {
+                            break;
+                        }
+                        nx = x + 1;
+                        ny = y;
+                    }
+                    Direction::LEFT => {
+                        if y as i32 - 1 < 0 {
+                            break;
+                        }
+                        nx = x;
+                        ny = y - 1;
+                    }
+                }
+                match inner_data[nx][ny] {
+                    '#' => d = d.turn(),
+                    'X' | '.' => {
+                        x = nx;
+                        y = ny;
+                        steps += 1;
+                    }
+                    _ => exit(1),
+                }
+
+                if steps > data.len() * data.len() * data.len() {
+                    res += 1;
+                    break;
+                }
+            }
+        }
     }
+
     res
 }
