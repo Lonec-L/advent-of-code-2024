839a9e6257dc4755b7f58179d65eae3cdab96f85
Luka Lonec <luka.lonec@gmail.com>
2025-10-20 11:02:38 +0200

    test: test test

---
+++ c/include/math_utils.h
@@ -0,0 +1,11 @@
+
+
+/*
+ * Calculates the greatest commont divisor using euclidean algorithm
+ */
+long long gcd(long long a, long long b);
+
+/*
+ * Calculates lowest common denominator using GCD
+ */
+long long lcd(long long a, long long b);
